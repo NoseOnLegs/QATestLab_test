7958cff3207bf08fe792225f4203bb2757e60941
NoseOnLegs <illia.karpachov@qateslab.net>
2022-09-12 14:39:55 +0300

    Selenoid

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
 
 from app.app import App
 
@@ -10,8 +8,18 @@ PORTAL_URL = 'http://prestashop-automation.qatestlab.com.ua/uk/'
 
 @pytest.fixture(scope="session")
 def app():
-    service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(service=service)
+    capabilities = {
+        "browserName": "firefox",
+        "browserVersion": "104.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        desired_capabilities=capabilities)
     app = App(driver)
     app.driver.get(PORTAL_URL)
     yield app
