58986de279339221d366771e4f26c02288e33a03
NoseOnLegs <illia.karpachov@qateslab.net>
2022-09-13 12:22:48 +0300

    Jenkins integration

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,9 +5,13 @@ pipeline {
             steps {
                 sh 'pip install -r requirements.txt'
                 sh 'python3 -m pytest --alluredir=allure-results test.py'
-                sh 'zip -r results.zip allure-results'
-                sh 'mv results.zip $HOME/Documents'
             }
+        }
+    }
+    post {
+        always {
+            sh 'zip -r results.zip allure-results'
+            sh 'mv results.zip $HOME/Documents'
         }
     }
 }
--- conftest.py
+++ conftest.py
@@ -1,26 +1,24 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 from app.app import App
 
 PORTAL_URL = 'http://prestashop-automation.qatestlab.com.ua/uk/'
-pytest_plugins = 'allure'
 
 
 @pytest.fixture(scope="session")
 def app():
-    capabilities = {
-        "browserName": "firefox",
-        "browserVersion": "",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
+    options = Options()
+    options.set_capability('browserName', 'firefox')
+    options.set_capability('selenoid:options',
+                           {"enableVNC": True,
+                            "enableVideo": False})
 
     driver = webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub",
-        desired_capabilities=capabilities)
+        options=options
+    )
     app = App(driver)
     app.driver.get(PORTAL_URL)
     yield app
